d474e65143186bf893fee125e8fac6925482e2d1
wchao190 <wchao190@163.com>
2021-04-24 20:02:27 +0800

    对指定字符串排序

--- src/com/sgg/TestThread/StringTest.java
+++ src/com/sgg/TestThread/StringTest.java
@@ -6,14 +6,15 @@ package com.sgg.TestThread;
 public class StringTest {
 
     public String reverse(String str,int startIndex,int endIndex){
-        String newStr = str.substring(startIndex,endIndex);
+        String str1 = str.substring(0,startIndex);
+        String str3 = str.substring(endIndex);
+        String str2 = str.substring(startIndex,endIndex);
         StringBuilder sb = new StringBuilder();
-        for(int i=newStr.length()-1;i>=0;i--){
-            sb.append(newStr.charAt(i));
+            for(int i=str2.length()-1;i>=0;i--){
+            sb.append(str2.charAt(i));
         }
-        //return new String(sb);
-        return sb.toString();
-    }
+        return str1+sb.toString()+str3;
+   }
     public static void main(String[] args) {
 
         String str = "abcdefg";
